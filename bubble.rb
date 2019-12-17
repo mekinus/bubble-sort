ff6963414abd1f3064057f82c67dbdcc5850a6f4
Philippe Odoubadé <philippejide@outlook.com>
2019-12-17 23:24:50 +0100

    added bubble_sort_by method

--- bubble.rb
+++ bubble.rb
@@ -26,4 +26,46 @@ end
 
 sorted_numbers = bubble_sort([30,200,70,80])
 
-puts sorted_numbers
+puts sorted_numbers.join(" ")
+
+#######################################
+
+
+def compare_by_length(left,right)
+  value = 0
+  if left.length > right.length
+  value = 1
+    elsif left.length == right.length
+    value = 0
+    else
+    value = -1
+  end
+  return value
+end
+
+
+
+puts compare_by_length("Suliva","Sulivan")
+
+def bubble_sort_by(array)
+count = array.length
+index = 0
+elements =0
+while index < count - 1
+  while elements < count - index - 1
+   if yield(array[elements],array[elements + 1]) > 0
+    left_numb = array[elements]
+    right_numb = array[elements + 1]
+    array[elements] = right_numb
+    array[elements + 1] = left_numb
+    end
+   elements += 1
+  end
+index += 1
+end
+new_sorted_array = array
+return new_sorted_array
+end
+
+
+puts bubble_sort_by(["hi","hello","hey"]) {|str1,str2|compare_by_length(str1,str2)} 
